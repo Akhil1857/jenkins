9837c5c1999bf04c2e276a0a55d5a7039517a254
Akhil1857 <125340453+Akhil1857@users.noreply.github.com>
2023-02-20 22:39:47 +0530

    Update sum.java

--- sum.java
+++ sum.java
@@ -1,6 +1,6 @@
 public class sum {
     public static void main(String[] args) {
-        int a=10,b=134;
+        int a=10,b=120;
         int c=a+b;
         System.out.println("The sum of two number is: " +c);
 
